b2ace3aa25ceb9b21e21bbd4634e62c719716974
Alesya <kalesiaq@gmail.com>
2023-02-22 20:24:05 +0300

    доработала негативный тест

--- test_searching.py
+++ test_searching.py
@@ -6,6 +6,7 @@ def test_searching(browser_size, clear_field):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('In ancient Greek mythology and religion'))
 
+
 def test_neg(browser_size, clear_field):
-    browser.element('[name="q"]').type('dota 2').press_enter()
-    browser.element('[id="search"]').should(have.text('doctor who'))
+    browser.element('[name="q"]').type('qwertrewqweqwqwewqwefghjkloiuytrewsdfghjk').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('ничего не найдено.'))
